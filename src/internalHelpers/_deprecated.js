b507df1aee95f2c2c8c60b61dbe13c456562640c
Brian Hough <brian.r.hough@gmail.com>
2017-05-25 12:30:05 +0200

    fix(messageHandlers): Base messageHandlers implementation with tests

---
+++ src/internalHelpers/_deprecated.js
@@ -0,0 +1,8 @@
+const deprecated = {
+  "mixins/clearFix.js": {
+    version: "3.0",
+    guidance: "You should use the messageHandler property directly instead.",
+  },
+}
+
+export default deprecated
